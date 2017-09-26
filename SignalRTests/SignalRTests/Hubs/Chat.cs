4aea4007c4acd76d87c0013f38d892c136336a06
Jarzembowski <jargemboski@gmail.com>
2017-09-26 11:51:37 -0300

    inital Commit

---
+++ SignalRTests/SignalRTests/Hubs/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRTests.Hubs
+{
+    public class Chat : Hub
+    {
+        public void EnviarMensagem(string apelido, string mensagem)
+        {           
+            Clients.All.PublicarMensagem(apelido, mensagem);
+        }
+
+    }
+}
